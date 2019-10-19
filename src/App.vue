2e8a8a4c0285e9a3a8fb31239340c0ed7916d53f
Tallerr <malashta@gmail.com>
2019-10-19 12:47:34 +0700

    query event

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <header class="">
-      <search-bar />
+      <search-bar :placeholderTxt="'Enter an activity..'" />
     </header>
     <section class="container">
       <todo-list />
@@ -19,6 +19,79 @@ export default {
     SearchBar,
     Pagination,
     TodoList
-  }
+  },
+	data() {
+  	return {
+  		todoInputStr: ''
+		}
+	}
 };
 </script>
+
+<style lang="scss">
+  @import "~reset-css/reset.css";
+
+  body {
+    background: #edf0f1;
+    padding: 80px 0 0 0;
+  }
+
+  body, input, button {
+    font-family: 'Roboto', sans-serif;
+  }
+
+  .noFill {
+    fill: none;
+  }
+
+  header {
+    width: 100%;
+    height: 80px;
+
+    position: fixed;
+    padding: 15px;
+    top: 0;
+    left: 0;
+    z-index: 5;
+
+    background: #25b99a;
+    box-shadow: 0px 2px 4px rgba(44, 62, 80, 0.15);
+    border-bottom-right-radius: 10px;
+    border-bottom-left-radius: 10px;
+  }
+
+  header button {
+    width: 50px;
+    height: 50px;
+
+    position:absolute;
+    top:15px;
+    right:15px;
+    z-index:2;
+
+    border-radius: 25px;
+    background: #fff;
+    border: 0px;
+    box-shadow: none;
+    outline: none;
+    cursor: pointer;
+
+    -webkit-appearance: none;
+    -moz-appearance: none;
+  }
+
+  header button svg {
+    width: 16px;
+    height: 16px;
+
+    position: absolute;
+    top: 50%;
+    left: 50%;
+
+    margin: -8px 0 0 -8px;
+  }
+
+  header button svg .fill {
+    fill: #25b99a;
+  }
+</style>
--- src/components/search-bar/search-bar.vue
+++ src/components/search-bar/search-bar.vue
@@ -1,11 +1,24 @@
 <template>
-  <div class="search-bar">
-    <pre v-text="$attrs"></pre>
-  </div>
+  <form class="search-bar" @submit.prevent="$emit('query', queryStr)">
+    <input type="text" v-model="queryStr" :placeholder="placeholderTxt">
+  </form>
 </template>
 
 <script>
 export default {
-  props: {}
+  props: {
+		placeholderTxt: {
+			type: String
+		}
+  },
+	data() {
+		return {
+			queryStr: ''
+		};
+	}
 };
 </script>
+
+<style lang="scss">
+  @import "search-bar";
+</style>
